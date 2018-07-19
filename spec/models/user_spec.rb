c0a566f38258d004f446c900275f3af7c1f14004
John Li <j@peer.com>
2018-07-18 22:02:41 -0600

    Remove all references to account_type

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, :instructor) }
+  # let(:course_taught) { create(:course, instructor: user) }
+  #
+  # before do
+  #   user.courses << course_taught
+  # end
 
   describe 'validations' do
     it "should be valid" do
@@ -16,6 +21,12 @@ describe User, type: :model do
 
     it "should not be valid without a last name" do
       user.update(last_name: nil)
+
+      expect(user).not_to be_valid
+    end
+
+    it "should not be valid without the correct email format" do
+      user.update(email: "john@adspectercom")
 
       expect(user).not_to be_valid
     end
