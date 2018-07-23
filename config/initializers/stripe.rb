daf06f1fe1d677d91f94bf505ee4b888e9bee14f
Jessica <jessicazhangq7@gmail.com>
2018-07-22 20:15:46 -0400

    Fix ENV variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
     #:publishable_key => ENV['PUBLISHABLE_KEY'],
     #:secret_key      => ENV['SECRET_KEY']
-    :publishable_key => ENV[pk_test_JfkJNk8SSeqeBrfmHhKBh4K6],
-    :secret_key      => ENV[sk_test_1IGSWRw5f2lM59WGIQqGgqcX]
+    :publishable_key => pk_test_JfkJNk8SSeqeBrfmHhKBh4K6,
+    :secret_key      => sk_test_1IGSWRw5f2lM59WGIQqGgqcX
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
