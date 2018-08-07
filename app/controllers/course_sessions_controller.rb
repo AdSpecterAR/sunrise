75b299d2f674f744d3c464643e266a2df72b8e9c
Jessica <jessicazhangq7@gmail.com>
2018-08-06 23:59:53 -0400

    Changes upcoming course window from one day to one week

--- app/controllers/course_sessions_controller.rb
+++ app/controllers/course_sessions_controller.rb
@@ -1,7 +1,7 @@
 class CourseSessionsController < ApplicationController
 
   def upcoming_courses
-    @course_sessions = CourseSession.courses_in_next_24_hours
+    @course_sessions = CourseSession.courses_in_next_week
     course_sessions_json = { upcoming_courses: CourseSessionRepresenter.for_collection.new(@course_sessions) }
 
     render json: { courses: course_sessions_json }
